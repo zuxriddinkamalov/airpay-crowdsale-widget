512b72a0c5999283337201d26bf4971d0c0bd3f8
Misha Yefimets <misha.efimets@gmail.com>
2018-10-04 10:06:31 +0300

    s

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-app = express()
-app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic('dist/'));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started ' + port);
