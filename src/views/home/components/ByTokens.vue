94493689816d17f905c00eaf9cd0e0583e8a03d2
Misha Yefimets <misha.efimets@gmail.com>
2018-10-13 11:59:44 +0300

    check

--- src/views/home/components/ByTokens.vue
+++ src/views/home/components/ByTokens.vue
@@ -6,7 +6,7 @@
         <el-form @submit.prevent.native ref="buyTokenForm" :rules="rulesByToken" :model="form">
             <el-form-item class="pledge input-with-button"
               prop="pledge">
-                <slot name="label"><div class="uppercase label">You pladge</div></slot>
+                <slot name="label"><div class="uppercase label">You pledge</div></slot>
                 <el-input min="1" type="number" v-model="form.pledge">
                     <el-select class="currency" v-model="form.currency" slot="append">
                         <el-option label="BTC" value="btc"></el-option>
@@ -24,11 +24,11 @@
                         </h3>
                     </el-col>
                     <el-col :xs="24" :sm="12">
-                        <slot name="label"><div class="uppercase gray label">Bonuses</div></slot>
+                        <slot name="label"><div class="uppercase gray label">current rate</div></slot>
                         <h3>
-                            <span class="bold">+{{$R.path(['settings', 'bonus'], airpay)}}%</span>
-                            <!--&nbsp;<span class="period uppercase">Ends in 21 days</span>-->
+                            <span class="summ uppercase">1 ETH = {{getRate($R.path(['settings', 'rate'], airpay))}} {{$R.path(['settings', 'name'], airpay)}}</span>
                         </h3>
+                        <span class="period uppercase">include bonus +{{$R.path(['settings', 'bonus'], airpay)}}%</span>
                     </el-col>
                 </el-row>
             </el-form-item>
@@ -79,14 +79,17 @@
 </template>
 
 <script>
-import { prop, path, map } from 'ramda'
-import { mapState } from 'vuex'
+import { prop, path, map } from 'ramda';
+import { mapState } from 'vuex';
 
-import VTextSlider from '@/components/TextSlider'
-import { BY_TOKENS_MUTATION, PERFORM_BUYING_MUTATION } from '../../../graphql/airpay/mutations'
-import { SET_AIRPAY_DATA } from '../../../store/modules/airpay/mutation-types'
-import { SET_GENERAL_DATA } from '../../../store/modules/general/mutation-types'
-import { prepareValidateErrors } from '../../../helpers/general'
+import VTextSlider from '@/components/TextSlider';
+import {
+  BY_TOKENS_MUTATION,
+  PERFORM_BUYING_MUTATION
+} from '../../../graphql/airpay/mutations';
+import { SET_AIRPAY_DATA } from '../../../store/modules/airpay/mutation-types';
+import { SET_GENERAL_DATA } from '../../../store/modules/general/mutation-types';
+import { prepareValidateErrors } from '../../../helpers/general';
 
 const SLIDER = [
   {
@@ -113,26 +116,30 @@ const SLIDER = [
     sum: 4.9,
     currency: 'BTC'
   }
-]
+];
 
 export default {
   name: 'ByTokens',
-  data: function () {
+  data: function() {
     let checkZero = (rule, value, callback) => {
       if (value === '') {
-        callback(new Error('Amount is required'))
+        callback(new Error('Amount is required'));
       } else if (value < 1) {
-        callback(new Error('Amount can not be less then one'))
+        callback(new Error('Amount can not be less then one'));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
     return {
       loading: false,
-      sliderText: map(item =>
-        `<img class="flag" src="${item.flag}" alt="" />
-        User from <span class="bold">${item.country}</span> pledge <span class="bold">${item.sum} ${item.currency}</span>`
-        , SLIDER),
+      sliderText: map(
+        item =>
+          `<img class="flag" src="${item.flag}" alt="" />
+        User from <span class="bold">${
+          item.country
+        }</span> pledge <span class="bold">${item.sum} ${item.currency}</span>`,
+        SLIDER
+      ),
       form: {
         pledge: null,
         email: '',
@@ -142,122 +149,135 @@ export default {
       },
       rulesByToken: {
         pledge: [
-          { validator: checkZero, message: 'Amount can not be less then one', trigger: 'blur' }
+          {
+            validator: checkZero,
+            message: 'Amount can not be less then one',
+            trigger: 'blur'
+          }
         ],
         email: [
           { required: true, message: 'Email required', trigger: 'blur' },
-          { type: 'email', message: 'Please enter valid email', trigger: 'blur' }
+          {
+            type: 'email',
+            message: 'Please enter valid email',
+            trigger: 'blur'
+          }
         ]
       }
-    }
+    };
   },
-  created () {
+  created() {
     // console.warn(this.$store.state)
   },
   methods: {
-    getSumm: function (rate) {
-      let price = parseFloat(prop('pledge', this.form)) * rate
+    getSumm: function(rate) {
+      let price = parseFloat(prop('pledge', this.form)) * rate;
       if (price) {
-        let val = (price).toFixed(0)
-        return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        let val = price.toFixed(0);
+        return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
       }
-      return '0'
+      return '0';
     },
-    submit: function () {
+    getRate: function(rate) {
+      return rate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    },
+    submit: function() {
       if (prop('hash', this.airpay)) {
-        this.verificate('verificateForm')
+        this.verificate('verificateForm');
       } else {
-        this.buyTokens('buyTokenForm')
+        this.buyTokens('buyTokenForm');
       }
     },
-    buyTokens (formName) {
+    buyTokens(formName) {
       this.$refs[formName].validate((valid, error) => {
         if (valid) {
-          this.loading = true
-          this.$apollo.mutate({
-            mutation: BY_TOKENS_MUTATION,
-            variables: {
-              email: this.form.email
-            }
-          }).then(response => {
-            this.$store.commit(`airpay/${SET_AIRPAY_DATA}`, {
-              ...this.$store.state.airpay,
-              hash: path(['data', 'enter'], response)
+          this.loading = true;
+          this.$apollo
+            .mutate({
+              mutation: BY_TOKENS_MUTATION,
+              variables: {
+                email: this.form.email
+              }
             })
-            this.loading = false
-          }).catch(response => {
-            this.loading = false
-          })
+            .then(response => {
+              this.$store.commit(`airpay/${SET_AIRPAY_DATA}`, {
+                ...this.$store.state.airpay,
+                hash: path(['data', 'enter'], response)
+              });
+              this.loading = false;
+            })
+            .catch(response => {
+              this.loading = false;
+            });
         } else {
-          let message = prepareValidateErrors(error)
+          let message = prepareValidateErrors(error);
           this.$message({
             dangerouslyUseHTMLString: true,
             type: 'error',
             message: message
-          })
-          return false
+          });
+          return false;
         }
-      })
+      });
     },
-    verificate (formName) {
-      let self = this
-      let enter = new Promise(function (resolve, reject) {
-        self.$refs['buyTokenForm'].validate(
-          (valid, error) => {
-            if (valid) {
-              resolve()
-            } else {
-              let message = prepareValidateErrors(error)
-              self.$message({
-                dangerouslyUseHTMLString: true,
-                type: 'error',
-                message: message
-              })
-            }
+    verificate(formName) {
+      let self = this;
+      let enter = new Promise(function(resolve, reject) {
+        self.$refs['buyTokenForm'].validate((valid, error) => {
+          if (valid) {
+            resolve();
+          } else {
+            let message = prepareValidateErrors(error);
+            self.$message({
+              dangerouslyUseHTMLString: true,
+              type: 'error',
+              message: message
+            });
           }
-        )
-      })
-      let verification = new Promise(function (resolve, reject) {
+        });
+      });
+      let verification = new Promise(function(resolve, reject) {
         self.$refs[formName].validate((valid, error) => {
           if (valid) {
-            resolve()
+            resolve();
           }
-        })
-      })
-      Promise.all([enter, verification]).then(function () {
-        self.loading = true
-        self.$apollo.mutate({
-          mutation: PERFORM_BUYING_MUTATION,
-          variables: {
-            hash: prop('hash', self.airpay),
-            code: '1111',
-            amount: self.form.pledge,
-            currency: self.form.currency
-          }
-        }).then(response => {
-          let data = path(['data', 'buyTokens'], response)
-          self.$store.commit(`airpay/${SET_AIRPAY_DATA}`, {
-            ...self.$store.state.airpay,
-            byTokenData: data
+        });
+      });
+      Promise.all([enter, verification]).then(function() {
+        self.loading = true;
+        self.$apollo
+          .mutate({
+            mutation: PERFORM_BUYING_MUTATION,
+            variables: {
+              hash: prop('hash', self.airpay),
+              code: '1111',
+              amount: self.form.pledge,
+              currency: self.form.currency
+            }
           })
-          sessionStorage.setItem('token', prop('authorization', data))
-          self.$store.commit(SET_GENERAL_DATA, 'VDeposit')
-          self.loading = false
-        }).catch(response => {
-          self.loading = false
-        })
-      })
+          .then(response => {
+            let data = path(['data', 'buyTokens'], response);
+            self.$store.commit(`airpay/${SET_AIRPAY_DATA}`, {
+              ...self.$store.state.airpay,
+              byTokenData: data
+            });
+            sessionStorage.setItem('token', prop('authorization', data));
+            self.$store.commit(SET_GENERAL_DATA, 'VDeposit');
+            self.loading = false;
+          })
+          .catch(response => {
+            self.loading = false;
+          });
+      });
     }
   },
   computed: {
-    ...mapState([
-      'airpay'
-    ])
+    ...mapState(['airpay'])
   },
   components: {
     VTextSlider
   }
-}
+};
 </script>
 
 <style lang="sass">
