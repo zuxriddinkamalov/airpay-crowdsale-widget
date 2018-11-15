3808e383743f91f8953a17f3aac0a3fc16129af7
Misha Yefimets <misha.efimets@gmail.com>
2018-11-15 12:55:48 +0200

    build

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic('dist/'));
+app.use(serveStatic(__dirname + 'dist/'));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);
