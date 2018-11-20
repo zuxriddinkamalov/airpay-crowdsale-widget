d99f29e7135b831b3db270b2f26f6bbd58d50fb1
Inspiron <bigjoss@mail.ru>
2018-11-20 21:21:00 +0500

    element-ui problem fixed

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,4 +1,4 @@
-export const API_HOST = 'airpay-network-api-stage.herokuapp.com';
+export const API_HOST = 'airpay-network-api-stage.herokuapp.com'
 // export const API_HOST = 'localhost:4000'
-export const API_URL = `https://${API_HOST}/`;
-export const API_WS_URL = `wss://${API_HOST}/graphql`;
+export const API_URL = `https://${API_HOST}/`
+export const API_WS_URL = `wss://${API_HOST}/graphql`
